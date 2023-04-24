511037beae80eb605fccffc4aa8f16e85d7c4f74
AGENCIA CRISTAL APPS <christian.udemy@gmail.com>
2023-04-23 19:42:56 -0500

    first commit

---
+++ schemas/banner.js
@@ -0,0 +1,65 @@
+export default {
+    name: 'banner',
+    title: 'Banner',
+    type: 'document',
+    fields: [
+        {
+            name: 'image',
+            title: 'Imagen',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: 'buttonText',
+            title: 'ButtonText',
+            type: 'string',
+        },
+        {
+            name: 'product',
+            title: 'Producto',
+            type: 'string',
+        },
+        {
+            name: 'desc',
+            title: 'Desc',
+            type: 'string',
+        },
+        {
+            name: 'smallText',
+            title: 'Texto corto',
+            type: 'string',
+        },
+        {
+            name: 'midText',
+            title: 'MidText',
+            type: 'string',
+        },
+        {
+            name: 'largeText1',
+            title: 'LargeText1',
+            type: 'string',
+        },
+        {
+            name: 'largeText2',
+            title: 'LargeText2',
+            type: 'string',
+        },
+        {
+            name: 'discount',
+            title: 'Discount',
+            type: 'string',
+        },
+        {
+            name: 'saleTime',
+            title: 'SaleTime',
+            type: 'string',
+        },
+        {
+            name: 'slug',
+            title: 'Ruta del producto',
+            type: 'string',
+        },
+    ],
+  };
